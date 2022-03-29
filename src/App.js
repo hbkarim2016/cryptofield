cf79f46782e8b77433033a38f4a3cea0bd13fbd2
hbkarim2016 <hbkarim2016@gmail.com>
2022-03-29 17:47:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import { useRef } from "react";
-import Header from "./app-contents/components/header/Header";
+
 import Home from "./app-contents/pages/home/Home";
 
 function App() {
 
-  const headerRef = useRef();
-  
-  window.addEventListener( 'scroll', () => {
-    if( headerRef !== undefined ){
-      if( window.pageYOffset > 10 ){
-        headerRef.current.classList.add('headerDownStyle')
-      }else{
-        headerRef.current.classList.remove('headerDownStyle')
-      }
-    }
-  })
-
   return (
     <div className="App">
-      <header ref={headerRef} className='test'>
-        <Header />
-      </header>
-      <div>
-        <Home />
-      </div>
+      <Home />
     </div>
   );
 }
--- src/app-contents/pages/home/Home.jsx
+++ src/app-contents/pages/home/Home.jsx
@@ -1,11 +1,28 @@
+import Header from "../../components/header/Header";
 import Banner from "../../components/banner/Banner";
 import Platform from "../../components/platform/Platform";
 import Process from "../../components/process/Process";
+import { useRef } from "react";
 import './home.css';
 import './home-responsive.css';
 const Home = () => {
+  
+    const headerRef = useRef();
+  
+    window.addEventListener( 'scroll', () => {
+        if( headerRef !== undefined ){
+          if( window.pageYOffset > 10 ){
+            headerRef.current.classList.add('headerDownStyle')
+          }else{
+            headerRef.current.classList.remove('headerDownStyle')
+          }
+        }
+      })
     return(
         <div>
+            <header ref={headerRef} className='test'>
+                <Header />
+            </header>
             <section className="banner-section">
                 <div className="banner-bg">
                     <Banner />
